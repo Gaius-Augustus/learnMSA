added AIC model selection, fixed overflow issues with inverse softmax

learnMSA/msa_hmm/Align.py
@@ -182,8 +182,12 @@ def run_learnMSA(train_filename,
         alignment.best_model = np.argmax(posterior_sums)
         if verbose:
             print("Per model total expected match states:", posterior_sums)
+    elif config["model_criterion"] == "AIC":
+        num_param = 34 * np.array(alignment.length) + 25
+        aic = -2 * alignment.loglik * fasta_file.num_seq + 2*num_param
+        alignment.best_model = np.argmax(aic)
     else:
-        raise SystemExit("Invalid model selection criterion. Valid criteria are loglik and posterior.") 
+        raise SystemExit("Invalid model selection criterion. Valid criteria are loglik, AIC and posterior.") 
     if verbose:
         likelihoods = ["%.4f" % ll + " (%.4f)" % p for ll,p in zip(alignment.loglik, alignment.prior)]
         print("Per model likelihoods (priors): ", likelihoods)

learnMSA/msa_hmm/AncProbsLayer.py
@@ -7,7 +7,9 @@ some amount of evolutionary time has passed under a substitution model. This can
 """
               
 def inverse_softplus(features):
-    return np.log(np.expm1(features))
+    #cast to float 64 to prevent overflow of large entries
+    features64 = features.astype(np.float64)
+    return np.log(np.expm1(features64)).astype(features.dtype)
 
 def parse_paml(lines, desired_alphabet):
     """Parses the content of a paml file.