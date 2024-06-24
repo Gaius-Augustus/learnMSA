fixed underflow in parallel posteriior probs

learnMSA/msa_hmm/Align.py
@@ -58,6 +58,7 @@ def fit_and_align(data : SequenceDataset,
                   subset=None,
                   initial_model_length_callback=get_initial_model_lengths,
                   sequence_weights=None,
+                  clusters=None,
                   verbose=True):
     assert_config(config)
     model_generator, batch_generator = _make_defaults_if_none(model_generator, batch_generator)
@@ -99,6 +100,7 @@ def fit_and_align(data : SequenceDataset,
                                           batch_size=batch_size, 
                                           epochs=epochs_this_iteration,
                                           sequence_weights=sequence_weights,
+                                          clusters=clusters,
                                           verbose=verbose)
         am = AlignmentModel(data, batch_generator, decode_indices, batch_size=batch_size, model=model)
         if last_iteration:
@@ -165,8 +167,9 @@ def run_learnMSA(data : SequenceDataset,
                  insertion_aligner="famsa",
                  aligner_threads=0,
                  sequence_weights=None,
+                 clusters=None,
                  verbose=True, 
-                  initial_model_length_callback=get_initial_model_lengths,
+                 initial_model_length_callback=get_initial_model_lengths,
                  select_best_for_comparison=True,
                  logo_gif_mode=False,
                  logo_dir=""):
@@ -203,6 +206,7 @@ def run_learnMSA(data : SequenceDataset,
                                 subset=subset, 
                                 initial_model_length_callback=initial_model_length_callback,
                                 sequence_weights=sequence_weights,
+                                clusters=clusters,
                                 verbose=verbose)
         if verbose:
             print("Time for alignment:", "%.4f" % (time.time()-t_a))
@@ -610,7 +614,7 @@ def do_model_surgery(iteration, am : AlignmentModel, config, emission_dummy, tra
 
 
 #computes clustering based sequence weights if mmseqs2 is installed
-def compute_sequence_weights(fasta_filename, directory, cluster_seq_id=0.5):
+def compute_sequence_weights(fasta_filename, directory, cluster_seq_id=0.5, return_clusters=False):
     if which("mmseqs") is None:
         print("mmseqs2 is not installed or not in PATH. Consider installing it with conda install -c bioconda mmseqs2 or disable sequence weighting.")
         sys.exit(1)
@@ -633,6 +637,7 @@ def compute_sequence_weights(fasta_filename, directory, cluster_seq_id=0.5):
         cluster_counts = clustering.groupby("representative").size().to_frame("cluster_size")
         clustering = clustering.merge(cluster_counts, how="left", on="representative")
         clustering["weight"] = 1/clustering["cluster_size"]
+        clustering["cluster_index"] = clustering.groupby("representative").ngroup()
         clustering = clustering.set_index("sequence")
 
         with SequenceDataset(fasta_filename, "fasta") as data:
@@ -646,8 +651,11 @@ def compute_sequence_weights(fasta_filename, directory, cluster_seq_id=0.5):
                     if pos != -1:
                         ids[i] = ids[i][pos+1:]
             sequence_weights = np.array(clustering.loc[ids].weight, dtype=np.float32)
-            
-        return sequence_weights
+        if return_clusters:
+            clusters = np.array(clustering.loc[ids].cluster_index, dtype=np.int32)
+            return sequence_weights, clusters
+        else:
+            return sequence_weights
 
 
 def get_model_scores(am, model_criterion, verbose):

learnMSA/msa_hmm/AncProbsLayer.py
@@ -117,6 +117,9 @@ class AncProbsLayer(tf.keras.layers.Layer):
         equilibrium_sample: If true, a 2-staged process is assumed where an amino acid is first sampled from 
                         the equilibirium distribution and the ancestral probabilities are computed afterwards.
         transposed: Transposes the probability matrix P = e^tQ. 
+        clusters: An optional vector that assigns each sequence to a cluster. If provided, the evolutionary time
+                    is learned per cluster.
+        use_lstm: Experimental setting that estimates the evolutionary distance of a sequence with an lstm.
         name: Layer name.
     """
 
@@ -135,6 +138,8 @@ class AncProbsLayer(tf.keras.layers.Layer):
                  shared_matrix=False,
                  equilibrium_sample=False,
                  transposed=False,
+                 clusters=None,
+                 use_lstm=False,
                  **kwargs):
         super(AncProbsLayer, self).__init__(**kwargs)
         self.num_models = num_models
@@ -151,12 +156,23 @@ class AncProbsLayer(tf.keras.layers.Layer):
         self.shared_matrix = shared_matrix
         self.equilibrium_sample = equilibrium_sample
         self.transposed = transposed
+        self.clusters = clusters
+        self.num_clusters = np.max(clusters) + 1 if clusters is not None else self.num_rates
+        self.use_lstm = use_lstm
     
     def build(self, input_shape=None):
-        self.tau_kernel = self.add_weight(shape=[self.num_models, self.num_rates], 
-                                   name="tau_kernel", 
-                                   initializer=self.rate_init,
-                                   trainable=self.trainable_distances)
+        if self.use_lstm:
+            self.lstm_dim = 64
+            self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.lstm_dim, return_sequences=True, zero_output_for_mask=True), merge_mode="sum")
+            self.dense = tf.keras.layers.Dense(1, 
+                                            activation="softplus",
+                                            #kernel_initializer="zeros",
+                                            bias_initializer=self.rate_init)
+        else:
+            self.tau_kernel = self.add_weight(shape=[self.num_models, self.num_clusters], 
+                                    name="tau_kernel", 
+                                    initializer=self.rate_init,
+                                    trainable=self.trainable_distances)
         if self.shared_matrix:
             self.exchangeability_kernel = self.add_weight(shape=[self.num_models, 1, 20, 20],
                                                           name="exchangeability_kernel",
@@ -182,6 +198,7 @@ class AncProbsLayer(tf.keras.layers.Layer):
             self.per_matrix_rates_kernel = self.add_weight(shape=[self.num_models, self.num_matrices], 
                                                       name="per_matrix_rates_kernel",
                                                       initializer=self.matrix_rate_init)
+
        
     def make_R(self, kernel=None):
         if kernel is None:
@@ -202,11 +219,27 @@ class AncProbsLayer(tf.keras.layers.Layer):
         Q = tf.reshape(Q, (self.num_models, self.num_matrices, 20, 20))
         return Q
         
-    def make_tau(self, subset=None):
-        tau = self.tau_kernel
-        if subset is not None:
-            tau = tf.gather_nd(tau, subset, batch_dims=1)
-        return tf.math.softplus(tau)
+    def make_tau(self, inputs=None, subset=None):
+        if self.use_lstm:
+            if len(inputs.shape) == 3:
+                num_model, b, L = tf.unstack(tf.shape(inputs))
+            else:
+                num_model, b, L, _ = tf.unstack(tf.shape(inputs))
+            lstm_input = tf.one_hot(inputs, len(SequenceDataset.alphabet))
+            lstm_input = tf.reshape(lstm_input, (num_model*b, L, len(SequenceDataset.alphabet)))
+            lstm_mask = tf.reshape(inputs < len(SequenceDataset.alphabet)-1, (num_model*b, L))
+            seq_lens = tf.reduce_sum(tf.cast(lstm_mask, lstm_input.dtype), axis=-1, keepdims=True)
+            lstm_output = self.lstm(lstm_input, mask=lstm_mask)
+            lstm_output = tf.reduce_sum(lstm_output, axis=-2) / (seq_lens * self.lstm_dim)
+            lstm_output = tf.reshape(lstm_output, (num_model, b, self.lstm_dim))
+            return self.dense(lstm_output)[...,0]
+        else:
+            tau = self.tau_kernel
+            if self.clusters is not None:
+                tau = tf.gather(tau, self.clusters, axis=-1)
+            if subset is not None:
+                tau = tf.gather_nd(tau, subset, batch_dims=1)
+            return tf.math.softplus(tau)
     
     def make_per_matrix_rate(self):
         return tf.math.softplus(self.per_matrix_rates_kernel)
@@ -224,15 +257,16 @@ class AncProbsLayer(tf.keras.layers.Layer):
         rate_indices.set_shape([self.num_models,None]) #resolves tf 2.12 issues
         input_indices = len(inputs.shape) == 3 
         if input_indices:
+            num_model, b, L = tf.unstack(tf.shape(inputs))
             mask = inputs < 20
             only_std_aa_inputs = inputs * tf.cast(mask, inputs.dtype)
             mask = tf.cast(mask, self.dtype)
             mask = tf.expand_dims(mask, -1)
             mask = tf.expand_dims(mask, -1)
         else:
+            num_model, b, L, _ = tf.unstack(tf.shape(inputs))
             only_std_aa_inputs = inputs
-        rate_indices = tf.expand_dims(rate_indices, -1)
-        tau_subset = self.make_tau(rate_indices)
+        tau_subset = self.make_tau(inputs, tf.expand_dims(rate_indices, -1))
         if self.per_matrix_rate:
             per_matrix_rates = self.make_per_matrix_rate()
             per_matrix_rates = tf.expand_dims(per_matrix_rates, 1)
@@ -242,8 +276,9 @@ class AncProbsLayer(tf.keras.layers.Layer):
             self.add_loss(self.matrix_rate_l2 * reg)
         else:
             mut_rates = tau_subset
-        reg_tau = tf.reduce_sum(tf.square(self.tau_kernel + 3.))
-        self.add_loss(self.matrix_rate_l2 * reg_tau)
+        if not self.use_lstm:
+            reg_tau = tf.reduce_sum(tf.square(self.tau_kernel + 3.))
+            self.add_loss(self.matrix_rate_l2 * reg_tau)
         anc_probs = make_anc_probs(only_std_aa_inputs, 
                                    self.make_R(), 
                                    self.make_p(), 
@@ -255,10 +290,8 @@ class AncProbsLayer(tf.keras.layers.Layer):
             anc_probs = tf.pad(anc_probs, [[0,0], [0,0], [0,0], [0,0], [0,len(SequenceDataset.alphabet)-20]])
             rest = tf.expand_dims(tf.one_hot(inputs, len(SequenceDataset.alphabet)), -2) * (1-mask)
             anc_probs += rest
-            num_model, b, L = tf.unstack(tf.shape(inputs))
             anc_probs = tf.reshape(anc_probs, (num_model, b, L, self.num_matrices * len(SequenceDataset.alphabet)) )
         else:
-            num_model, b, L, _ = tf.unstack(tf.shape(inputs))
             anc_probs = tf.reshape(anc_probs, (num_model, b, L, self.num_matrices * 20) )
         return anc_probs
         
@@ -278,7 +311,9 @@ class AncProbsLayer(tf.keras.layers.Layer):
              "matrix_rate_l2" : self.matrix_rate_l2,
              "shared_matrix" : self.shared_matrix,
              "equilibrium_sample" : self.equilibrium_sample,
-             "transposed" : self.transposed
+             "transposed" : self.transposed,
+                "clusters" : self.clusters,
+             "use_lstm" : self.use_lstm
         })
         return config
     

learnMSA/msa_hmm/MsaHmmLayer.py
@@ -123,7 +123,7 @@ class MsaHmmLayer(tf.keras.layers.Layer):
         #compute the actual forward variables across the chunks via the total probability
         forward_chunks_last = forward_chunks[:,:,-1]  #(num_model*b, factor, q, q)
         forward_chunks_last = tf.reshape(forward_chunks_last, (num_model*b, self.parallel_factor, q*q))
-        forward_total, _, loglik = self.total_prob_rnn(tf.math.exp(forward_chunks_last)) #(num_model*b, factor, q)
+        forward_total, _, loglik = self.total_prob_rnn(forward_chunks_last) #(num_model*b, factor, q)
         init, _ = self.cell.get_initial_state(batch_size=b, parallel_factor=1)
         init = tf.math.log(init + self.cell.epsilon)
         T = tf.concat([init[:,tf.newaxis], forward_total[:,:-1]], axis=1)
@@ -185,7 +185,7 @@ class MsaHmmLayer(tf.keras.layers.Layer):
         #compute the actual backward variables across the chunks via the total probability
         backward_chunks_last = backward_chunks[:,:,0]  #(num_model*b, factor, q, q)
         backward_chunks_last = tf.reshape(backward_chunks_last, (num_model*b, self.parallel_factor, q*q))
-        backward_total, _, _ = self.total_prob_rnn_rev(tf.math.exp(backward_chunks_last)) #(num_model*b, factor, q)
+        backward_total, _, _ = self.total_prob_rnn_rev(backward_chunks_last) #(num_model*b, factor, q)
         backward_total = tf.reverse(backward_total, [1])
         init, _ = self.reverse_cell.get_initial_state(batch_size=b, parallel_factor=1)
         init = tf.math.log(init + self.reverse_cell.epsilon)

learnMSA/msa_hmm/TotalProbabilityCell.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 
 class TotalProbabilityCell(tf.keras.layers.Layer):
     """ A utility RNN cell that computes the total forward probabilities based on the conditional
-        forward probabilities of chunked observations given the state at their left border.
+        forward probabilities of chunked observations given the state at their left border 
+        in logarithmic scale to avoid underflow.
     Args:
         cell: HMM cell whose forward recursion is used.
         reverse: If True, the cell is configured for computing the backward recursion.
@@ -22,24 +23,23 @@ class TotalProbabilityCell(tf.keras.layers.Layer):
 
     def call(self, conditional_forward, states, training=None, init=False):
         """ Args:
-            conditional_forward: A batch of conditional forward probabilities. Shape: (b, q*q)
+            conditional_forward: A batch of conditional logarithmic forward probabilities. Shape: (b, q*q)
             The rows correspond to the states that are conditioned on.
             Returns: The log of the total forward probabilities. Shape: (b, q)
         """
-        old_scaled_forward, old_loglik = states
+        old_forward, _ = states
         conditional_forward = tf.reshape(conditional_forward, (-1, self.cell.max_num_states, self.cell.max_num_states))
-        scaled_forward = tf.einsum("bc,bcq->bq", old_scaled_forward, conditional_forward)
-        scaled_forward = tf.maximum(scaled_forward, self.cell.epsilon)
-        S = tf.reduce_sum(scaled_forward, axis=-1)
-        loglik = old_loglik + tf.math.log(S)
-        scaled_forward /= S[:,tf.newaxis]
-        new_state = [scaled_forward, loglik]
-        output = tf.math.log(scaled_forward) + loglik[:,tf.newaxis]
+        forward = old_forward[...,tf.newaxis] + conditional_forward
+        forward = tf.math.reduce_logsumexp(forward, axis=-2)
+        loglik = tf.math.reduce_logsumexp(forward, axis=-1)
+        new_state = [forward, loglik]
+        output = forward
+
         return output, new_state
     
     def get_initial_state(self, batch_size=None, inputs=None, dtype=None):
         if self.reverse:
-            init_dist = tf.ones((batch_size, self.cell.max_num_states), dtype=self.dtype)
+            init_dist = tf.zeros((batch_size, self.cell.max_num_states), dtype=self.dtype)
             loglik = tf.zeros((batch_size), dtype=self.dtype)
             S = [init_dist, loglik]
         else:
@@ -47,7 +47,7 @@ class TotalProbabilityCell(tf.keras.layers.Layer):
             init_dist = tf.transpose(init_dist, (1,0,2))
             init_dist = tf.reshape(init_dist, (-1, self.cell.max_num_states))
             loglik = tf.zeros((batch_size), dtype=self.dtype)
-            S = [init_dist, loglik]
+            S = [tf.math.log(init_dist), loglik]
         return S
 
     def get_config(self):

learnMSA/msa_hmm/Training.py
@@ -54,7 +54,7 @@ def make_msa_hmm_layer(effective_num_seq,
                                 dtype=tf.float32)
     return msa_hmm_layer
 
-def make_anc_probs_layer(num_seq, config):
+def make_anc_probs_layer(num_seq, config, clusters=None):
     assert_config(config)
     anc_probs_layer = AncProbsLayer(config["num_models"],
                                     num_seq,
@@ -69,7 +69,8 @@ def make_anc_probs_layer(num_seq, config):
                                      matrix_rate_l2=config["matrix_rate_l2"],
                                      shared_matrix=config["shared_rate_matrix"],
                                      equilibrium_sample=config["equilibrium_sample"],
-                                     transposed=config["transposed"])
+                                     transposed=config["transposed"],
+                                     clusters=clusters)
     return anc_probs_layer
 
 def default_model_generator(num_seq,
@@ -78,6 +79,7 @@ def default_model_generator(num_seq,
                             config,
                             data : SequenceDataset = None,
                             sequence_weights=None,
+                            clusters=None,
                             alphabet_size=len(SequenceDataset.alphabet)-1,
                             generic_gen=generic_model_generator):
     """A callback that constructs the default learnMSA model. Can be used as a template for custom generators.
@@ -89,6 +91,7 @@ def default_model_generator(num_seq,
         data: The sequence dataset corresponding to this model. Typically but not necessarily equal to the training data of the model. 
               This is part of the model generator callback because the generated model might depends on some sequence information like maximum length.
         sequence_weights: Optional likelihood weights per sequence.
+        clusters: Optional cluster indices for each sequence.
         alphabet_size: Number of symbols without the terminal symbol.
     """
     num_models = config["num_models"]
@@ -96,7 +99,7 @@ def default_model_generator(num_seq,
         (f"The list of given model lengths ({len(model_lengths)}) should"
          + f" match the number of models specified in the configuration({num_models}).")
     msa_hmm_layer = make_msa_hmm_layer(effective_num_seq, model_lengths, config, sequence_weights, alphabet_size)
-    anc_probs_layer = make_anc_probs_layer(num_seq, config)
+    anc_probs_layer = make_anc_probs_layer(num_seq, config, clusters)
     model = generic_gen([anc_probs_layer], msa_hmm_layer)
     return model
 
@@ -220,6 +223,7 @@ def fit_model(model_generator,
               batch_size, 
               epochs,
               sequence_weights=None,
+              clusters=None,
               verbose=True, 
               train_callbacks=[]):
     assert_config(config)
@@ -245,7 +249,8 @@ def fit_model(model_generator,
                                 model_lengths=model_lengths,
                                 config=config,
                                 data=data,
-                                sequence_weights=sequence_weights)
+                                sequence_weights=sequence_weights,
+                                clusters=clusters)
         model.compile(optimizer=optimizer)
         return model
     num_gpu = len([x.name for x in tf.config.list_logical_devices() if x.device_type == 'GPU']) 