0df7fea8b0b3b4f88502f1cde2f8cca2649bc0d6
Ung0d <beckerfelix94@gmail.com>
2026-04-17 09:08:51 +0000

    damp automatic batch size scaling with large GPUs

--- learnMSA/model/training_util.py
+++ learnMSA/model/training_util.py
@@ -5,7 +5,7 @@ from learnMSA.run.util import get_batch_multiplicator, get_avail_memory_bytes
 
 DEFAULT_IMPL_FACTOR = 0.25
 MAX_BATCH_SIZE = 2048
-MEMORY_DAMP = 0.7
+MEMORY_DAMP = 0.5
 
 
 def get_initial_model_lengths(
